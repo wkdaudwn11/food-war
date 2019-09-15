28b2c3f6eb989535a5f5c0b561feab6e79afb7db
JangMyungJoo <wkdaudwn11@naver.com>
2019-09-15 17:12:44 +0900

    CORS solved (only requests from github, localhost)

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import cors from 'cors'; // CORS를 허용하기 위한 모듈
 
 /** Import router */
 import userRouter from './router/userRouter';
@@ -16,11 +17,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
+/** CORS 관련 코드 시작 */
+var whitelist = ['https://wkdaudwn11.github.io'];
+var corsOptions = {
+  origin: function(origin, callback) {
+    // whiteList에 속하거나, 자기자신일 경우엔 CORS 허용
+    if (whitelist.indexOf(origin) !== -1 || origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+/** CORS 관련 코드 시작 */
+
 /** Routing */
 app.use('/user', userRouter);
 
 const PORT = process.env.PORT || 4000;
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}..`);
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}..`);
 
 app.listen(PORT, err => {
   if (err) {
